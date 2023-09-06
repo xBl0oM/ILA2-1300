d18a992dd95a786e326bceac9d57f7c8e1837a19
xBl0oM <111044215+xBl0oM@users.noreply.github.com>
2023-09-06 10:00:07 +0200

    Add files via upload

---
+++ Code/Tutorial.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RnGeezus
+{
+    /// <summary>
+    /// Interaktionslogik für Tutorial.xaml
+    /// </summary>
+    public partial class Tutorial : Window
+    {
+        public Tutorial()
+        {
+            InitializeComponent();
+            TutorialSong.Source = new Uri(@"C:\Schule\BBB\2.Jahr\ila\1300\RnGeezus\RnGeezus\Free Bird (low quality) (mp3cut.net).mp3");
+            TutorialSong.Play();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
---
+++ Code/Welcome.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RnGeezus
+{
+    /// <summary>
+    /// Interaktionslogik für Welcome.xaml
+    /// </summary>
+    public partial class Welcome : Window
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+            StartSong.Source = new Uri(@"C:\Schule\BBB\2.Jahr\ila\1300\RnGeezus\RnGeezus\Forever Bound - Stereo Madness.mp3");
+            StartSong.Play();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void WelcomeStartTutorial_Click(object sender, RoutedEventArgs e)
+        {
+            Tutorial tutorial = new Tutorial();
+            tutorial.Show();
+            Close();
+        }
+    }
+}
